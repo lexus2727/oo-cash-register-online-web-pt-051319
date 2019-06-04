f5674851f7c60755dc4c530eca9925f19832b29d
Candice Parker <lexus2424@gmail.com>
2019-06-04 13:05:16 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,18 @@
-
+class CashRegister
+  
+  attr_accessor :total, :discount,  :items
+  def initialization(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []
+  end
+  
+  def add_items(title, price, quantity = 1)
+    
+    
+  end
+  
+  
+  
+  
+end
